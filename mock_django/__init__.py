f321bb46e5f19cbede8e9ac73afba484585b391f
Dmitry Lazarko <lazarko@adwz.ru>
2013-12-26 11:28:27 +0400

    Remove http.py

--- mock_django/__init__.py
+++ mock_django/__init__.py
@@ -5,8 +5,7 @@ mock_django
 :copyright: (c) 2012 DISQUS.
 :license: Apache License 2.0, see LICENSE for more details.
 """
-
-from .http import *
+from .query import *
 from .managers import *
 from .models import *
 from .signals import *
